d8c31b0cbfdcdc53cbd71409338f0bf41384ff6a
arbaaz-77 <43441056+arbaaz-77@users.noreply.github.com>
2021-08-08 13:40:28 +0300

    full build

---
+++ script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const endgame = '7 April 2022';
+
+function countdown() {
+    const finishDate = new Date(endgame).getTime();
+    const currentDate = new Date().getTime();
+
+    const  timeleft = finishDate - currentDate;
+    
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
